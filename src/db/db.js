92803d68078cf84a7cd27a93ebe76fcfb24213d4
Sumon Mondal <sumonmondal2003@gmail.com>
2025-09-04 20:20:23 +0530

    update db file

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-// db function setup
+
 function connectDB(){
     mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("Database connected successfully");
         
     }).catch((err)=>{
-        console.log("Database connection failed");
+        console.log("Database connection failed .");
         console.log(err);
     })
 }
